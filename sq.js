a6536ee616e969006c15509e87c8dad8960daa8c
Michal Budzowski <Michal.Budzowski@sonarsource.com>
2025-10-09 11:01:50 +0200

    add sq.js

---
+++ sq.js
@@ -0,0 +1,7 @@
+const props = new Map();
+props.set("user_token", "squ_b4556a16fa2d28519d2451a911d2e073024010bc");
+props.set("project_analysis", "sqp_ab0d92954f7ff6f95b12e8469a741e11e835c389");
+props.set("global_analysis", "sqa_e3a16514bb5d5fb27035fe130bcc7c8a365566f5");
+props.set("ibm-key", "fDKU7e_u_EnQgWgDVO4b_ubGqVTa5IYwWEey7lMfEB_1");
+props.set("tcol", "1:wieDoh8nah3aus/0deepheez7ph+och7daix0ophaeM=");
+props.set("nnak", "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");

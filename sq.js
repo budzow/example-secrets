3601d455eaeb25555626cd7c78077cbf8daba9d0
Michal Budzowski <Michal.Budzowski@sonarsource.com>
2025-10-09 12:06:08 +0200

    add variations of ibm keys

--- sq.js
+++ sq.js
@@ -4,5 +4,8 @@ props.set("project_analysis", "sqp_ab0d92954f7ff6f95b12e8469a741e11e835c389");
 props.set("global_analysis", "sqa_e3a16514bb5d5fb27035fe130bcc7c8a365566f5");
 props.set("ibm-key", "fDKU7e_u_EnQgWgDVO4b_ubGqVTa5IYwWEey7lMfEB_1");
 props.set("ibm-key", "fDKU7e_u_EnQgWgDVO4b_ubGqVTa5IYwWEey7lMfEB+1");
+props.set("ibm_key", "fDKU7e_u_EnQgWgDVO4b_ubGqVTa5IYwWEey7lMfEB+1");
+props.set("ibmkey", "fDKU7e_u_EnQgWgDVO4b_ubGqVTa5IYwWEey7lMfEB+1");
+props.set("ibmkey1", "fDKU7e_u_EnQgWgDVO4b_ubGqVTa5IYwWEey7lMfEB+1");
 props.set("tcol", "1:wieDoh8nah3aus/0deepheez7ph+och7daix0ophaeM=");
 props.set("nnak", "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");
